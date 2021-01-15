744b9d3a1eeb8bf34e1ee220257915868ecbcf4e
Uwe Vogt <haumea@uv-software.de>
2021-01-15 17:09:48 +0100

    [CANAPI-18] Applied an intermediate solution to build_no.bat

--- api/build_no.bat
+++ api/build_no.bat
@@ -5,7 +5,7 @@ echo  *  project   :  CAN - Controller Area Network >> build_no.h
 echo  * >> build_no.h
 echo  *  purpose   :  CAN Interface API, Version 3 (PCAN-Basic) >> build_no.h
 echo  * >> build_no.h
-echo  *  copyright :  (C) 2018-2020, UV Software, Berlin >> build_no.h
+echo  *  copyright :  (C) 2018-2021, UV Software, Berlin >> build_no.h
 echo  * >> build_no.h
 echo  *  compiler  :  Microsoft Visual C/C++ Compiler (Version 19.16) >> build_no.h
 echo  * >> build_no.h
@@ -20,8 +20,8 @@ echo  * >> build_no.h
 echo  */ >> build_no.h
 echo #ifndef BUILD_NO_H_INCLUDED >> build_no.h
 echo #define BUILD_NO_H_INCLUDED >> build_no.h
-echo #define BUILD_NO \>> build_no.h
-svn info --show-item last-changed-revision can_api.c >> build_no.h
+rem FIXME: get pseudo build number from git hash
+echo #define BUILD_NO 0x0000000 >> build_no.h
 echo #define STRINGIFY(X) #X >> build_no.h
 echo #define TOSTRING(X) STRINGIFY(X) >> build_no.h
 echo #define SVN_REV_INT (BUILD_NO) >> build_no.h
