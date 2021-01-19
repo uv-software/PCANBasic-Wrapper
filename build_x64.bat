9be03a8107e818a02ccd26d4bd604e5f0573964f
UweVgt <haumea@uv-software.de>
2021-01-19 20:54:00 +0100

    Add build scripts for Visual Studio 2017 Community Edition
    Relates to [CANAPI-25]

---
+++ build_x64.bat
@@ -0,0 +1,34 @@
+@echo off
+
+pushd
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" x64
+popd
+
+pushd
+call build_no.bat
+rem type .\Sources\build_no.h
+rem pause
+
+call msbuild.exe .\Trial\pcb_test.vcxproj /t:Clean;Build /p:"Configuration=Debug";"Platform=x64"
+if errorlevel 1 goto end
+
+call msbuild.exe .\Libraries\CANAPI\uvcanpcb.vcxproj /t:Clean;Build /p:"Configuration=Release";"Platform=x64"
+if errorlevel 1 goto end
+
+echo Copying artifacts...
+set BIN=".\Binaries"
+if not exist %BIN% mkdir %BIN%
+set BIN=".\Binaries\x64"
+if not exist %BIN% mkdir %BIN%
+copy .\Libraries\CANAPI\x64\Release\u3canpcb.dll %BIN%
+copy .\Libraries\CANAPI\x64\Release\u3canpcb.lib %BIN%
+
+set INC=".\Includes"
+if not exist %INC% mkdir %INC%
+copy .\Sources\PCAN.h %INC%
+copy .\Sources\CANAPI\can_api.h %INC%
+copy .\Sources\CANAPI\CANAPI*.h %INC%
+
+:end
+popd
+pause
---
+++ build_x86.bat
@@ -0,0 +1,34 @@
+@echo off
+
+pushd
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat" x86
+popd
+
+pushd
+call build_no.bat
+rem type .\Sources\build_no.h
+rem pause
+
+call msbuild.exe .\Trial\pcb_test.vcxproj /t:Clean;Build /p:"Configuration=Debug";"Platform=Win32"
+if errorlevel 1 goto end
+
+call msbuild.exe .\Libraries\CANAPI\uvcanpcb.vcxproj /t:Clean;Build /p:"Configuration=Release";"Platform=Win32"
+if errorlevel 1 goto end
+
+echo Copying artifacts...
+set BIN=".\Binaries"
+if not exist %BIN% mkdir %BIN%
+set BIN=".\Binaries\x86"
+if not exist %BIN% mkdir %BIN%
+copy .\Libraries\CANAPI\Release\u3canpcb.dll %BIN%
+copy .\Libraries\CANAPI\Release\u3canpcb.lib %BIN%
+
+set INC=".\Includes"
+if not exist %INC% mkdir %INC%
+copy .\Sources\PCAN.h %INC%
+copy .\Sources\CANAPI\can_api.h %INC%
+copy .\Sources\CANAPI\CANAPI*.h %INC%
+
+:end
+popd
+pause
