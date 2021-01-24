98bf5a9354f59d2b6515aab9a30749c6ca7f6f57
UweVgt <haumea@uv-software.de>
2021-01-24 21:43:22 +0100

    Add two simple C++ examples
    Relates to [CANAPI-25]

---
+++ Examples/C++/can_recv.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <signal.h>
+#include <errno.h>
+
+#include "PCAN.h"
+
+#ifndef CHANNEL
+#define CHANNEL  0x51
+#endif
+#ifndef BAUDRATE
+#define BAUDRATE  CANBTR_INDEX_250K
+#endif
+
+static void sigterm(int signo);
+static volatile int running = 1;
+
+static CPCAN myDriver = CPCAN();
+
+int main(int argc, const char * argv[]) {
+    CANAPI_OpMode_t opMode = {};
+    opMode.byte = CANMODE_DEFAULT;
+    CANAPI_Bitrate_t bitrate = {};
+    bitrate.index = BAUDRATE;
+    CANAPI_Message_t message = {};
+    CANAPI_Return_t retVal = 0;
+    int frames = 0;
+
+    std::cout << CPCAN::GetVersion() << std::endl;
+    if((signal(SIGINT, sigterm) == SIG_ERR) ||
+#if !defined(_WIN32) && !defined(_WIN64)
+       (signal(SIGHUP, sigterm) == SIG_ERR) ||
+#endif
+       (signal(SIGTERM, sigterm) == SIG_ERR)) {
+        perror("+++ error");
+        return errno;
+    }
+    if ((retVal = myDriver.InitializeChannel(CHANNEL, opMode)) != CCANAPI::NoError) {
+        std::cerr << "+++ error: interface could not be initialized" << std::endl;
+        return retVal;
+    }
+    if ((retVal = myDriver.StartController(bitrate)) != CCANAPI::NoError) {
+        std::cerr << "+++ error: interface could not be started" << std::endl;
+        goto teardown;
+    }
+    std::cout << "Press Ctrl+C to abort..." << std::endl;
+    while (running) {
+        if ((retVal = myDriver.ReadMessage(message, CANREAD_INFINITE)) == CCANAPI::NoError) {
+            fprintf(stdout, "%i\t", frames++);
+            fprintf(stdout, "%7li.%04li\t", (long)message.timestamp.tv_sec, message.timestamp.tv_nsec / 100000);
+            if (!opMode.fdoe)
+                fprintf(stdout, "%03X\t%c%c [%u] ", message.id, message.xtd ? 'X' : 'S', message.rtr ? 'R' : ' ', message.dlc);
+            else
+                fprintf(stdout, "%03X\t%c%c%c%c%c [%u] ", message.id, message.xtd ? 'X' : 'S', message.rtr ? 'R' : ' ',
+                        message.fdf ? 'F' : ' ', message.brs ? 'B' : ' ', message.esi ? 'E' :' ', CCANAPI::DLc2Len(message.dlc));
+            for (uint8_t i = 0; i < CCANAPI::DLc2Len(message.dlc); i++)
+                fprintf(stdout, " %02X", message.data[i]);
+            if (message.sts)
+                fprintf(stdout, " <<< status frame");
+            fprintf(stdout, "\n");
+        }
+        else if (retVal != CCANAPI::ReceiverEmpty) {
+            fprintf(stderr, "+++ error: read message returned %i", retVal);
+            running = 0;
+        }
+    }
+    std::cout << std::endl;
+teardown:
+    if ((retVal = myDriver.TeardownChannel()) != CCANAPI::NoError)
+        std::cerr << "+++ error: interface could not be shutdown" << std::endl;
+    return retVal;
+}
+
+static void sigterm(int signo) {
+    //fprintf(stderr, "%s: got signal %d\n", __FILE__, signo);
+    myDriver.SignalChannel();
+    running = 0;
+    (void)signo;
+}
---
+++ Examples/C++/can_send.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#if !defined(_WIN32) && !defined(_WIN64)
+#include <unistd.h>
+#else
+#include <windows.h>
+#endif
+#define OPTION_CANAPI_DRIVER  1
+#include "can_api.h"
+
+#ifndef CHANNEL
+#define CHANNEL  0x52
+#endif
+#ifndef BAUDRATE
+#define BAUDRATE  CANBTR_INDEX_250K
+#endif
+#define FRAMES  (CAN_MAX_STD_ID+1)
+
+int main(int argc, const char * argv[]) {
+    int handle, result, i;
+    can_bitrate_t bitrate;
+    can_message_t message;
+
+    std::cout << can_version() << std::endl;
+    if ((handle = can_init(CHANNEL, CANMODE_DEFAULT, NULL)) < 0) {
+        std::cerr << "+++ error: interface could not be initialized" << std::endl;
+        return -1;
+    }
+    bitrate.index = BAUDRATE;
+    if ((result = can_start(handle, &bitrate)) < 0) {
+        std::cerr << "+++ error: interface could not be started" << std::endl;
+        goto end;
+    }
+    std::cout << ">>> Be patient..." << std::flush;
+    message.xtd = message.rtr = message.sts = 0;
+    for (i = 0; i < FRAMES; i++) {
+        message.id = (uint32_t)i & CAN_MAX_STD_ID;
+        message.dlc = 8U;
+        message.data[0] = (uint8_t)((uint64_t)i >> 0);
+        message.data[1] = (uint8_t)((uint64_t)i >> 8);
+        message.data[2] = (uint8_t)((uint64_t)i >> 16);
+        message.data[3] = (uint8_t)((uint64_t)i >> 24);
+        message.data[4] = (uint8_t)((uint64_t)i >> 32);
+        message.data[5] = (uint8_t)((uint64_t)i >> 40);
+        message.data[6] = (uint8_t)((uint64_t)i >> 48);
+        message.data[7] = (uint8_t)((uint64_t)i >> 56);
+        if ((result = can_write(handle, &message, 0U)) < 0) {
+            std::cerr << "+++ error: interface could not be stopped" << std::endl;
+            goto reset;;
+        }
+    }
+#if !defined(_WIN32) && !defined(_WIN64)
+    usleep(1000000);  // afterburner
+#else
+    Sleep(1000);    // wait a minute
+#endif
+reset:
+    std::cout << i << " frame(s) sent" << std::endl;
+    if ((result = can_reset(handle)) < 0)
+        std::cerr << "+++ error: interface could not be stopped" << std::endl;
+end:
+    if ((result = can_exit(handle)) < 0)
+        std::cerr << "+++ error: interface could not be shutdown" << std::endl;
+    return result;
+}
