3220602544464c49c3959eb68d292a151cab4f32
Uwe Vogt <uwe.vogt@uv-software.de>
2024-08-01 21:11:33 +0200

    Set version number for release
    Relates to [CANAPI-107]

--- Sources/Version.h
+++ Sources/Version.h
@@ -52,7 +52,7 @@
 #ifdef _MSC_VER
 #define VERSION_MAJOR    0
 #define VERSION_MINOR    5
-#define VERSION_PATCH    99
+#define VERSION_PATCH    2
 #else
 #define VERSION_MAJOR    0
 #define VERSION_MINOR    3
