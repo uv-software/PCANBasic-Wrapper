d3c35c83d3250da32672eb31cf0ffe56d1e5ff77
Uwe Vogt <uwe.vogt@uv-software.de>
2026-09-21 17:56:28 +0200

    Restore Win32 DLL targets
    The DLLs are required by some MacCAN test suites.
    
    Relates to [CANAPI-118]

--- Sources/PeakCAN.cpp
+++ Sources/PeakCAN.cpp
@@ -68,6 +68,8 @@
 
 #if defined(_WIN64)
 #define PLATFORM        "x64"
+#elif defined(_WIN32)
+#define PLATFORM        "x86"
 #elif defined(__linux__)
 #define PLATFORM        "Linux"
 #elif defined(__APPLE__)
--- Sources/Wrapper/can_api.c
+++ Sources/Wrapper/can_api.c
@@ -59,6 +59,8 @@
 #endif
 #if defined(_WIN64)
 #define PLATFORM  "x64"
+#elif defined(_WIN32)
+#define PLATFORM  "x86"
 #else
 #error Platform not supported
 #endif
